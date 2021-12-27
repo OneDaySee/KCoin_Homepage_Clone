146bdfa85cbec67322aaa1e2f9b5bce53ec25688
OneDaySee <onderbirgungor@gmail.com>
2021-12-27 15:56:50 +0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'coin',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
